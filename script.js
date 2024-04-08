5f1587ec4fef923d85dad5fab0c9086815b8ba38
cosm1cgirl <166348211+cosm1cgirl@users.noreply.github.com>
2024-04-09 01:48:23 +0200

    images added
    images added, js and css updated

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 alert("Are you ready to see more?");
+document.getElementById("yesButton").addEventListener("click", function () {
+  document.getElementById("yesImage").style.display = "inline-block";
+  document.getElementById("noImage").style.display = "none";
+});
+
+document.getElementById("noButton").addEventListener("click", function () {
+  document.getElementById("noImage").style.display = "inline-block";
+  document.getElementById("yesImage").style.display = "none";
+});
